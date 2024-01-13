495de0f4bfeb9859c78fe6c0fd639c36af6c3028
javeria-farooq <gettingeducation6@gmail.com>
2024-01-13 20:53:20 +0500

    1st JS Assignment

---
+++ app.js
@@ -0,0 +1,72 @@
+// let name1 = "Javeria"
+// let name2 = "Aleeza"
+// let name3 = "Afnan"
+
+// let age1 = "20"
+// let age2 = "17"
+// let age3 = "10"
+
+// let fatherName1 = "Farooq"
+// let fatherName2 = "Farooq"
+// let fatherName3 = "Farooq"
+
+// let roll_no1 = "1234"
+// let roll_no2 = "1234"
+// let roll_no3 = "1234"
+
+// document.write(
+//     '<table border>' + 
+//     '<tr>' + 
+//     '<th>' + "Name" + '</th>'+
+//     '<th>' + "age" + '</th>'+
+//     '<th>' + "Father Name" + '</th>'+
+//     '<th>' + "Roll No" + '</th>' 
+//     + '</tr>'+
+
+//     '<tr>' + 
+//     '<td>' + name1 + '</td>'+
+//     '<td>' + age1 + '</td>'+
+//     '<td>' + fatherName1 + '</td>'+
+//     '<td>' + roll_no1 + '</td>' 
+//     + '</tr>'
+    
+//     + '</table>'
+// )
+
+// document.write(
+//     '<table border>' + 
+//     '<tr>' + 
+//     '<th>' + "Name" + '</th>'+
+//     '<th>' + "age" + '</th>'+
+//     '<th>' + "Father Name" + '</th>'+
+//     '<th>' + "Roll No" + '</th>' 
+//     + '</tr>'+
+
+//     '<tr>' + 
+//     '<td>' + name2 + '</td>'+
+//     '<td>' + age2 + '</td>'+
+//     '<td>' + fatherName2 + '</td>'+
+//     '<td>' + roll_no2 + '</td>' 
+//     + '</tr>'
+    
+//     + '</table>'
+// )
+
+// document.write(
+//     '<table border>' + 
+//     '<tr>' + 
+//     '<th>' + "Name" + '</th>'+
+//     '<th>' + "age" + '</th>'+
+//     '<th>' + "Father Name" + '</th>'+
+//     '<th>' + "Roll No" + '</th>' 
+//     + '</tr>'+
+
+//     '<tr>' + 
+//     '<td>' + name3 + '</td>'+
+//     '<td>' + age3 + '</td>'+
+//     '<td>' + fatherName3 + '</td>'+
+//     '<td>' + roll_no3 + '</td>' 
+//     + '</tr>'
+    
+//     + '</table>'
+// )
